d9abcbb1740f3e7adef7cfb42e027d5859dcfc74
steevenm888 <steevenm888@gmail.com>
2021-02-15 12:06:46 -0500

    Creation of the project with the base structure

---
+++ src/main/java/ec/edu/espe/banquito/crm/posibles/clientes/CrmPosiblesClientesApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.banquito.crm.posibles.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmPosiblesClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmPosiblesClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/banquito/crm/posibles/clientes/CrmPosiblesClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.banquito.crm.posibles.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmPosiblesClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
